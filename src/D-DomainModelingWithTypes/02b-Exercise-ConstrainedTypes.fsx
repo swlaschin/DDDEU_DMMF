d7b469a78d4197cdb7a879db4fb647f922c7f575
swlaschin <swlaschin@gmail.com>
2019-01-28 21:16:12 +0000

    Day 1

---
+++ src/D-DomainModelingWithTypes/02b-Exercise-ConstrainedTypes.fsx
@@ -0,0 +1,63 @@
+﻿open System
+
+//----------------------------------------------------------
+//  Q. Create a `NonZeroInteger`  type that can only contain non-zero integers
+
+module ConstrainedTypes =
+
+    type NonZeroInteger = private NonZeroInteger of int
+
+    module NonZeroInteger =
+        /// Public constructor
+        let create i =
+            ??
+
+        /// Return the value
+        let value ??  =
+            ??
+
+open ConstrainedTypes
+
+// test
+let x = NonZeroInteger.create 0
+
+
+//----------------------------------------------------------
+//  Q. Create a `ZipCode`  type that can only contain digit chars
+
+open System
+
+module ConstrainedTypes2 =
+
+    type ZipCode = private ZipCode of string
+
+    module ZipCode =
+        /// Public constructor
+        let create (s:string) =
+            let isAllDigits = s |> Seq.forall Char.IsDigit
+            if (s.Length = 5) && isAllDigits then
+                Some (ZipCode s)
+            else
+                None
+
+        /// Return the value
+        let value (ZipCode str) = str
+
+
+
+// test
+open ConstrainedTypes2
+
+let x2 = ZipCode.create "1234a
+ZipCode "aaaa"
+
+
+let value = x2 |> Option.get  // dont do this except for testing!
+
+let x3 = ZipCode.create "abc"
+
+
+
+
+
+
---
+++ src/D-DomainModelingWithTypes/04c3 - FSM Exercise 2 - Shipments (answers).fsx
@@ -0,0 +1,142 @@
+﻿// ================================================
+// FSM Exercise: Modeling package delivery transitions
+//
+// See Shipments transition diagram.png
+///
+// ================================================
+
+(*
+Exercise: create types that model package delivery transitions
+
+Rule: "You can't put a package on a truck if it is already out for delivery"
+Rule: "You can't sign for a package that is already delivered"
+
+States are:
+* Undelivered
+* OutForDelivery
+* Delivered
+*)
+
+open System
+
+module ShipmentsDomain =
+
+    // 1) Start with the domain types that are independent of state
+
+    type Package = string // placeholder for now
+    type TruckId = int
+    type SentUtc = DateTime
+    type DeliveredUtc = DateTime
+    type Signature = string
+
+    // 2) Create types to represent the data stored for each state
+
+    type DeliveryAttempt = DeliveryAttempt of Package * SentUtc
+    type UndeliveredData =
+        UndeliveredData of (Package * DeliveryAttempt list)
+    type OutForDeliveryData =
+        OutForDeliveryData of Package * TruckId * SentUtc * DeliveryAttempt list
+    type DeliveredData =
+        DeliveredData of Package * Signature * DeliveredUtc
+
+    // 3) Create a type that represent the choice of all the states
+
+    type Shipment =
+        | UndeliveredState of UndeliveredData
+        | OutForDeliveryState of OutForDeliveryData
+        | DeliveredState of DeliveredData
+
+    // 4) Create transition functions that transition from one state type to another
+
+    let sendOutForDelivery (UndeliveredData (package, attempts)) (truckId:TruckId)  :Shipment =
+        let utcNow = System.DateTime.UtcNow
+        // return new state
+        let newAttempt = DeliveryAttempt (package,utcNow)
+        let newAttempts = newAttempt :: attempts
+        let outForDeliveryData = OutForDeliveryData  (package, truckId, utcNow, newAttempts)
+        OutForDeliveryState outForDeliveryData
+
+    let addressNotFound (OutForDeliveryData (package, truckId, utcNow, attempts)) :Shipment =
+        // return new state
+
+        let undeliveredData = UndeliveredData (package, attempts)
+        UndeliveredState undeliveredData
+
+    let signedFor (OutForDeliveryData (package, truckId, utcNow, attempts)) (signature:Signature) :Shipment =
+        let utcNow = System.DateTime.UtcNow
+        // return new state
+        let deliveredData = DeliveredData (package,signature,utcNow)
+        DeliveredState deliveredData
+
+
+// ================================================
+// Now write some client code that uses this API
+// ================================================
+
+module ShipmentsClient =
+    open ShipmentsDomain
+
+    let putShipmentOnTruck (truckId:TruckId) state =
+        match state with
+        | UndeliveredState package ->
+            sendOutForDelivery package truckId
+        | OutForDeliveryState _ ->
+            printfn "package already out"
+            // return original state
+            state
+        | DeliveredState _ ->
+            printfn "package already delivered"
+            // return original state
+            state
+
+    let markAsDelivered (signature:Signature) state =
+        match state with
+        | UndeliveredState _  ->
+            printfn "package not out"
+            // return original state
+            state
+        | OutForDeliveryState data ->
+            signedFor data signature
+        | DeliveredState _ ->
+            printfn "package already delivered"
+            // return original state
+            state
+
+    let markAddressNotFound state =
+        match state with
+        | UndeliveredState _  ->
+            printfn "package not out"
+            // return original state
+            state
+        | OutForDeliveryState data ->
+            addressNotFound data
+        | DeliveredState _ ->
+            printfn "package already delivered"
+            // return original state
+            state
+
+
+
+// ================================================
+// Now write some test code
+// ================================================
+
+open ShipmentsDomain
+open ShipmentsClient
+
+let package = "My Package"
+let newShipment = UndeliveredState (UndeliveredData (package,[]))
+
+let truckId = 123
+let outForDelivery =
+    newShipment |> putShipmentOnTruck truckId
+
+let signature = "Scott"
+let delivered =
+    outForDelivery |> markAsDelivered signature
+
+// errors when using the wrong state
+delivered |> markAsDelivered signature
+delivered |> putShipmentOnTruck truckId
+
+outForDelivery |> markAddressNotFound
